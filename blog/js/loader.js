81eb74fd91341f312413e050234bd4fde877e1e9
Defective <def3ctive4@gmail.com>
2024-05-14 20:34:26 +0200

    No posts notice

--- blog/js/loader.js
+++ blog/js/loader.js
@@ -3,52 +3,77 @@ const xhr = new XMLHttpRequest();
 xhr.onreadystatechange = () => {
     if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
         const posts = JSON.parse(xhr.responseText)["posts"];
-        for (let index = 0; index < posts.length; index++) {
-            const post = posts[index];
-            const title = post["title"];
-            const time = post["time"];
-            const desc = post["desc"];
-            const link = post["link"];
-            const mmt = moment.unix(time);
-            const intval = mmt.fromNow();
+        if (posts.length == 0) {
 
             const card = document.createElement("div");
             const cardBody = document.createElement("div");
             const cardTitle = document.createElement("h5");
-            const cardTime = document.createElement("p");
             const cardText = document.createElement("p");
-            const cardLink = document.createElement("button");
 
             cardBody.classList.add("card-body");
 
-            cardTitle.innerHTML = title;
-            cardTime.innerHTML = intval;
-            cardTime.title = mmt.format("ddd, Do MMM YYYY [at] HH:mm");
-            cardTime.setAttribute("data-bs-toggle", "tooltip");
-            cardTime.setAttribute("data-bs-placement", "right");
-            cardText.innerHTML = desc.join("<br>");
-            cardLink.innerHTML = `Read more <i class="fa fa-arrow-right"
-            style="margin: 0px;"></i>`;
+            cardTitle.innerHTML = "No posts yet...";
+            cardText.innerHTML = "Please come back later!";
 
-            cardLink.classList.add("card-link");
-            cardLink.setAttribute("href", link);
             cardTitle.classList.add("card-title");
-            cardTime.classList.add("card-time");
             cardText.classList.add("card-text");
 
 
             cardBody.appendChild(cardTitle);
-            cardBody.appendChild(cardTime);
             cardBody.appendChild(cardText);
-            cardBody.appendChild(cardLink);
 
 
             card.classList.add("card");
             card.style.maxWidth = "50%";
             card.appendChild(cardBody);
             container.appendChild(card);
+        } else {
+            for (let index = 0; index < posts.length; index++) {
+                const post = posts[index];
+                const title = post["title"];
+                const time = post["time"];
+                const desc = post["desc"];
+                const link = post["link"];
+                const mmt = moment.unix(time);
+                const intval = mmt.fromNow();
 
-            attach(cardLink);
+                const card = document.createElement("div");
+                const cardBody = document.createElement("div");
+                const cardTitle = document.createElement("h5");
+                const cardTime = document.createElement("p");
+                const cardText = document.createElement("p");
+                const cardLink = document.createElement("button");
+
+                cardBody.classList.add("card-body");
+
+                cardTitle.innerHTML = title;
+                cardTime.innerHTML = intval;
+                cardTime.title = mmt.format("ddd, Do MMM YYYY [at] HH:mm");
+                cardTime.setAttribute("data-bs-toggle", "tooltip");
+                cardTime.setAttribute("data-bs-placement", "right");
+                cardText.innerHTML = desc.join("<br>");
+                cardLink.innerHTML = `Read more <i class="fa fa-arrow-right" style="margin: 0px;"></i>`;
+
+                cardLink.classList.add("card-link");
+                cardLink.setAttribute("href", link);
+                cardTitle.classList.add("card-title");
+                cardTime.classList.add("card-time");
+                cardText.classList.add("card-text");
+
+
+                cardBody.appendChild(cardTitle);
+                cardBody.appendChild(cardTime);
+                cardBody.appendChild(cardText);
+                cardBody.appendChild(cardLink);
+
+
+                card.classList.add("card");
+                card.style.maxWidth = "50%";
+                card.appendChild(cardBody);
+                container.appendChild(card);
+
+                attach(cardLink);
+            }
         }
         var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
         var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
